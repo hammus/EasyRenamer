49c8ef32e4683a588d1e2232067b255c6d4ae2a3
Liam Whan <liam.whan@gmail.com>
2016-05-25 11:16:51 +1000

    Complete

---
+++ EasyRenamer/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace EasyRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.OrigFilename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NewFilename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(45, 307);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(90, 29);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Browse";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OrigFilename,
+            this.NewFilename});
+            this.dataGridView1.Location = new System.Drawing.Point(45, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(937, 187);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // OrigFilename
+            // 
+            this.OrigFilename.HeaderText = "Original FileName";
+            this.OrigFilename.Name = "OrigFilename";
+            this.OrigFilename.ReadOnly = true;
+            this.OrigFilename.Width = 105;
+            // 
+            // NewFilename
+            // 
+            this.NewFilename.HeaderText = "New Filename";
+            this.NewFilename.Name = "NewFilename";
+            this.NewFilename.Width = 91;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(892, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rename Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 446);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrigFilename;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NewFilename;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EasyRenamer/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyRenamer
+{
+    public partial class Form1 : Form
+    {
+        private string _selectedPath; 
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            folderBrowserDialog1.SelectedPath = @"D:\Respite Upload\";
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                button1.Visible = true;
+                _selectedPath = folderBrowserDialog1.SelectedPath;
+
+                AddFilesToGrid();
+            }
+        }
+
+        private void AddFilesToGrid()
+        {
+            FileInfo[] files = GetAllFiles(_selectedPath);
+
+            foreach (FileInfo file in files)
+            {
+                dataGridView1.Rows.Add(file, file);
+
+            }
+        }
+
+        public static FileInfo[] GetAllFiles(string path)
+        {
+            try
+            {
+                DirectoryInfo root = new DirectoryInfo(path);
+                return root.GetFiles("*.*");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            catch (DirectoryNotFoundException e)
+            {
+                MessageBox.Show(e.Message);
+
+            }
+
+            return null;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value != row.Cells[1].Value)
+                {
+                    Debug.WriteLine("Renaming {0} to {1}", row.Cells[0].Value, row.Cells[1].Value);
+                    string origFile = Path.Combine(_selectedPath, row.Cells[0].Value.ToString());
+                    string newFile = Path.Combine(_selectedPath, row.Cells[1].Value.ToString());
+                    try
+                    {
+                        File.Move(origFile, newFile);
+                    } catch (IOException ex)
+                    {
+                        MessageBox.Show("File not found: " + origFile + "\nSystem Error Message: " + ex.Message, "Error Renaming File");
+                    } 
+                    
+                }
+                
+            }
+
+            dataGridView1.Rows.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.All;
+            } else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var filePath in files)
+                {
+                    Debug.WriteLine(filePath);
+                }
+            }
+        }
+    }
+}
